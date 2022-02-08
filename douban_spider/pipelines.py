9b68438dee4ac8a09eb56673a1bf65b29c63fc28
sam ma <sam_newyork@163.com>
2022-02-08 23:16:29 +0800

    define scrapy pipeline to extract data to mysql database

--- douban_spider/pipelines.py
+++ douban_spider/pipelines.py
@@ -6,8 +6,53 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import pymysql as pymysql
 
 
 class DoubanSpiderPipeline:
+    """将抓取的数据写入到mysql数据库中"""
+    def process_item(self, item, spider):
+        return item
+
+    def __init__(self, mysql_uri, mysql_db, mysql_user, password):
+        self.mysql_uri = mysql_uri
+        self.mysql_db = mysql_db
+        self.mysql_user = mysql_user
+        self.password = password
+        self.connect = pymysql.connect(host=self.mysql_uri, user=self.mysql_user, password=self.password,
+                                       db=self.mysql_db, charset='utf8')
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mysql_uri=crawler.settings.get('MYSQL_URL'),
+            mysql_db=crawler.settings.get('MYSQL_DATABASE', 'douban_datahub'),
+            mysql_user=crawler.settings.get('MYSQL_USER'),
+            password=crawler.settings.get('MYSQL_PASSWORD')
+        )
+
+    def open_spider(self, spider):
+        cursor = self.connect.cursor()
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        self.db[self.collection_name].insert_one(ItemAdapter(item).asdict())
+        return item
+
+
+class MongoPipeline:
+    def process_item(self, item, spider):
+        return item
+
+
+class CsvPipeline:
+    def process_item(self, item, spider):
+        return item
+
+
+class GraphDataPipeline:
+    """从scrapy抓取的movie meta数据中抽取点、边表数据"""
     def process_item(self, item, spider):
         return item
--- storage/schema.sql
+++ storage/schema.sql
@@ -6,3 +6,30 @@ create table pages
     created timestamp default current_timestamp,
     primary key (id)
 ) engine=Innodb default charset='utf8';
+
+/* 创建douban_datahub的数据表，电影meta信息表、评论数据表 */
+create table movie_meta (
+    id long comment '电影id',
+    movie_name varchar(100) comment '电影名称',
+    year long comment '拍摄年份',
+    topics varchar(200) comment '主题分类',
+    writer varchar(60) comment '编剧',
+    director varchar(60) comment '导演',
+    actors varchar(200) comment '主要演员',
+    official_site varchar(100) comment '官方网站',
+    movie_making_zone varchar(60) comment '发行地',
+    IMDb varchar(60) comment '电影id信息',
+    movie_rate float comment '电影评分',
+    votes float comment '参与评分的人数'
+) engine=Innodb default charset='utf8' comment '电影元数据';
+
+create table comment (
+    data_cid long comment '影评id',
+    movie_id long comment '电影id',
+    user_url varchar(100) comment '豆瓣用户URL',
+    nick_name varchar(100) comment '用户昵称',
+    time varchar(60) comment '评论时间',
+    content text comment '评论内容',
+    stars long comment '星级（几颗星）',
+    valid_votes int comment '投票数（有用数）'
+) engine=Innodb default charset='utf8' comment '电影热评';
